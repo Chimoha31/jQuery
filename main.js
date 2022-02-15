87b263f731a45ee82186833a96c46db9281851fb
Chimoha31 <mokochii1108@gmail.com>
2022-02-14 21:17:10 -0800

    Accordion

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 "use strict";
 
 $(function () {
-  $("#circle").animate(
-    { marginLeft: "700px", marginTop: "150px", borderRadius: "0" },
-    { duration: 3000, 
-      easing: 'swing'}
-  );
+  $('.panel > .title'). on('click', function() {
+    $('+p', this).toggle('show');
+  })
+  // $("p:not(:first)").hide();
+  // $(".panel > .title").on("click", function () {
+  //   $('.panel > p').slideUp(500);
+  //   $('+p', this).slideDown(500);
+  // });
 });
